def94c9bd3d0da464d377bab13b3c0b9846523bf
fjusteja <fjusteja87@gmail.com>
2025-01-17 17:48:06 +0700

    test

---
+++ Function.py
@@ -0,0 +1,21 @@
+def nm(nama, nama2):
+    print(f'hai {nama} dan {nama2}')
+
+nm(nama = "Joko", nama2 = 'Susi')
+nm("joko","Eli")
+
+#-------------------------
+def square(sisi):
+    luas = sisi*sisi
+    print(f'Luas persegi dgn sisi {sisi}cm adalah {luas}cm2')
+square(5)
+square(6)
+square(7)
+
+#-------------------------
+def square2(sisi2):
+    luas2 = sisi2*sisi2
+    return(luas2)
+vv = square2(9)
+print(vv)
+
