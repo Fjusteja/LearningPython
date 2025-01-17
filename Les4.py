5bb8e6d5183e09952a254376ab9b5f0dc62f18e5
fjusteja <fjusteja87@gmail.com>
2025-01-17 17:48:20 +0700

    test

--- Les4.py
+++ Les4.py
@@ -5,7 +5,7 @@ print(list_var)
 list_var2 = [i for i in range(1,5)]
 print(list_var2)
 
-list_var3 = [i for i in range(0,51,10)]
+list_var3 = [i for i in range(20,0,-3)]
 print(list_var3)
 
 x = [1,2,3]
@@ -13,5 +13,9 @@ y = [10,20,30]
 xy= [a*b for a in x for b in y]
 print(xy)
 
-# list_var4 = ["ganjil" if i%2==1 for i in ]
-# print(list_var4)
+
+list_var4 = [i for i in x if i%2==1]
+print(list_var4)
+
+list_var5 = ["ganjil" if i%2==1 else i for i in x]
+print(list_var5)
